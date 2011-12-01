73b9e395d4b1e2d552ffc5dde04e63a04f1cd54b
nXqd <nxqd.inbox@gmail.com>
2011-12-01 11:55:20 +0700

    first init

---
+++ ktpm-bt5/Program.cs
@@ -0,0 +1,10 @@
+﻿using ktpm_bt5.helpers;
+
+namespace ktpm_bt5 {
+    internal class Program {
+        private static void Main() {
+            var strings = DataReader.Read();
+            DataParser.Run(strings);
+        }
+    }
+}
